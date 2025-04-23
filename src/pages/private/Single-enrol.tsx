31e9b2bb1a70167e4828d7842c7c487a563b883a
Lance Brixter Mendoza <lance.vizserve@gmail.com>
2025-04-23 17:00:13 +0800

    feat: add/adding-single-student-table

---
+++ src/pages/private/Single-enrol.tsx
@@ -0,0 +1,26 @@
+import PageMetaData from "@/components/page-metadata";
+import { SectionCards } from "@/components/private/dashboard/section-cards";
+import SingleEnrol from "@/components/private/enroll/Single-Enrol";
+import { STUDENT_PROFILE_TITLE_DESCRIPTION } from "@/data";
+import { useParams } from "react-router";
+
+function SingleEnroll() {
+  const { title, description } = STUDENT_PROFILE_TITLE_DESCRIPTION;
+  const params = useParams();
+
+  if (!params.id) {
+    return <h1>Student number is not defined!</h1>;
+  }
+
+  return (
+    <>
+      <PageMetaData title={title} description={description} />
+      <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6 px-4 md:px-6">
+        <SectionCards />
+        <SingleEnrol />
+      </div>
+    </>
+  );
+}
+
+export default SingleEnroll;
