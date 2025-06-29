e7cb7810893c772f5d021b2c4ea0235443753f06
ace.vizserve <ace.vizserve@gmail.com>
2025-06-29 14:11:31 +0800

    chore: correct student age validation spelling

--- src/components/private/enrol-student/tabs/student-information/student-details.tsx
+++ src/components/private/enrol-student/tabs/student-information/student-details.tsx
@@ -33,7 +33,7 @@ function StudentDetails() {
     const age = differenceInYears(new Date(), values.birthDay);
 
     if (age < 4) {
-      toast.info("Child must be at least 4 years old to enroll");
+      toast.info("Child must be at least 4 years old to enrol!");
       form.setError("birthDay", {
         type: "manual",
         message: "Child must be at least 4 years old",
