a8beadbd340a2f7e6056dd47d1358d04c130f759
ace.vizserve <ace.vizserve@gmail.com>
2025-07-03 09:43:55 +0800

    fix: responsiveness for small screens

--- src/components/private/enrol-student/enrollment-stepper.tsx
+++ src/components/private/enrol-student/enrollment-stepper.tsx
@@ -23,7 +23,7 @@ export default function EnrollmentStepper({ setShowEnrollmentProcess, academicYe
           <div className="w-full md:w-11/12 mx-auto">
             <InnerImageZoom hideCloseButton src={enrollmentProcess} className="w-full h-auto rounded-md" />
           </div>
-          <p className="text-sm text-balance -tracking-tighter">
+          <p className="text-xs sm:text-sm text-balance -tracking-tighter">
             I/We acknowledge that I/we have read and understood the enrolment process for Academic Year{" "}
             {academicYear.split("y")[1]} at HFSE International School. I/We agree to follow the steps and requirements
             as outlined.
@@ -40,7 +40,7 @@ export default function EnrollmentStepper({ setShowEnrollmentProcess, academicYe
         <div className="space-y-4">
           <img src={discountPriceTag} className="max-h-64 md:max-h-72 lg:max-h-96 mx-auto w-max object-cover" />
 
-          <p className="text-sm text-balance -tracking-tighter">
+          <p className="text-xs sm:text-sm text-balance -tracking-tighter">
             I/We acknowledge that I/we have read and understood the terms and conditions of the school’s promos and
             discounts. I/We understand that failure to meet the stated terms or to settle payments on time will result
             in the forfeiture of any applicable promos or discounts.
