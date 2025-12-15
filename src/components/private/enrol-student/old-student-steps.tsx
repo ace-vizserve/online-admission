de3d69687246e5484ba4ab6857152c055221b870
ace.vizserve <ace.vizserve@gmail.com>
2025-12-15 16:44:01 +0800

    chore: add form tab state indicator for old student steps

--- src/components/private/enrol-student/old-student-steps.tsx
+++ src/components/private/enrol-student/old-student-steps.tsx
@@ -1,11 +1,21 @@
+import { Badge } from "@/components/ui/badge";
 import { buttonVariants } from "@/components/ui/button";
+import { useEnrolOldStudentContext } from "@/context/enrol-old-student-context";
 import { cn } from "@/lib/utils";
 import { useSelectAcademicYear } from "@/zustand-store";
 import { NavLink, useParams } from "react-router";
 
 function OldStudentSteps() {
+  const { formState } = useEnrolOldStudentContext();
   const params = useParams();
   const academicYear = useSelectAcademicYear((state) => state.academicYear);
+
+  const { familyInfo, enrollmentInfo, uploadRequirements } = formState;
+
+  const isFamilyInfoInvalid = familyInfo == null;
+  const enrollmentInfoIsInvalid = enrollmentInfo == null || enrollmentInfo.isValid !== true;
+  const studentEnrolmentDocsIsInvalid = uploadRequirements?.studentUploadRequirements.isValid !== true;
+  const parentGuardianEnrolmentDocsIsInvalid = uploadRequirements?.parentGuardianUploadRequirements.isValid !== true;
 
   return (
     <ol className="flex flex-col lg:flex-row gap-1 pb-6 lg:pb-0">
@@ -39,12 +49,14 @@ function OldStudentSteps() {
               "relative overflow-hidden lg:flex-1 w-full py-10 px-6 lg:rounded-t-none border lg:border-t-0",
               {
                 " bg-green-50 hover:bg-green-50 border-b-4 border-b-green-600 text-green-600 hover:text-green-600":
-                  isActive,
+                  isActive && !isFamilyInfoInvalid,
+                "border-b-4 border-b-primary": isActive && isFamilyInfoInvalid,
               }
             ),
             variant: "outline",
           })
         }>
+        {isFamilyInfoInvalid && <Badge className="text-[0.7rem] font-semibold absolute right-3 top-3">Review</Badge>}
         <div className="space-y-1 text-center px-6 py-4">
           <p className="text-sm font-semibold">Family Information</p>
           <p className="text-xs text-muted-foreground">Review Family information</p>
@@ -60,12 +72,18 @@ function OldStudentSteps() {
               "relative overflow-hidden lg:flex-1 w-full py-10 px-6 lg:rounded-t-none border lg:border-t-0",
               {
                 " bg-green-50 hover:bg-green-50 border-b-4 border-b-green-600 text-green-600 hover:text-green-600":
-                  isActive,
+                  isActive && !enrollmentInfoIsInvalid,
+                "border-b-4 border-b-destructive": isActive && enrollmentInfoIsInvalid,
               }
             ),
             variant: "outline",
           })
         }>
+        {enrollmentInfoIsInvalid && (
+          <Badge className="text-[0.7rem] font-semibold absolute right-3 top-3" variant={"destructive"}>
+            Unsaved
+          </Badge>
+        )}
         <div className="space-y-1 text-center px-6 py-4">
           <p className="text-sm font-semibold">Enrolment Information</p>
           <p className="text-xs text-muted-foreground">Review Student Enrolment details</p>
@@ -81,12 +99,21 @@ function OldStudentSteps() {
               "relative overflow-hidden lg:flex-1 w-full py-10 px-6 lg:rounded-t-none border lg:border-t-0",
               {
                 "bg-green-50 hover:bg-green-50 border-b-4 border-b-green-600 text-green-600 hover:text-green-600":
-                  isActive,
+                  isActive && !studentEnrolmentDocsIsInvalid && !parentGuardianEnrolmentDocsIsInvalid,
+
+                "border-b-4 border-b-destructive":
+                  isActive && (studentEnrolmentDocsIsInvalid || parentGuardianEnrolmentDocsIsInvalid),
               }
             ),
             variant: "outline",
           })
         }>
+        {studentEnrolmentDocsIsInvalid ||
+          (parentGuardianEnrolmentDocsIsInvalid && (
+            <Badge className="text-[0.7rem] font-semibold absolute right-3 top-3" variant={"destructive"}>
+              Unsaved
+            </Badge>
+          ))}
         <div className="space-y-1 text-center px-6 py-4">
           <p className="text-sm font-semibold">Upload Requirements</p>
           <p className="text-xs text-muted-foreground">Review your documents</p>
